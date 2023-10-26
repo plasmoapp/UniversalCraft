bead31acd1c1a518b6f1a4afac9dc675344793b5
Apehum <apehumchik@gmail.com>
2023-10-27 04:05:20 +0800

    Build: 1.20.2 forge

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,9 +2,11 @@ import gg.essential.gradle.util.*
 
 plugins {
     kotlin("jvm")
+    `java-library`
+    `maven-publish`
     id("gg.essential.multi-version")
     id("gg.essential.defaults")
-    id("gg.essential.defaults.maven-publish")
+//    id("gg.essential.defaults.maven-publish")
 }
 
 group = "gg.essential"
@@ -20,5 +22,31 @@ dependencies {
 tasks.jar {
     manifest {
         attributes(mapOf("FMLModType" to "LIBRARY"))
+    }
+}
+
+publishing {
+    publications {
+        register<MavenPublication>("maven") {
+            from(components["java"])
+
+            pluginManager.withPlugin("gg.essential.multi-version") {
+                val baseArtifactId = (if (parent == rootProject) rootProject.name.toLowerCase() else null)
+                    ?: project.findProperty("baseArtifactId")?.toString()
+                    ?: throw GradleException("No default base maven artifact id found. Set `baseArtifactId` in the `gradle.properties` file of the multi-version-root project.")
+                artifactId = "$baseArtifactId-$platform"
+            }
+        }
+    }
+
+    repositories {
+        maven("https://repo.plasmoverse.com/snapshots") {
+            name = "PlasmoVerseSnapshots"
+
+            credentials {
+                username = System.getenv("MAVEN_USERNAME")
+                password = System.getenv("MAVEN_PASSWORD")
+            }
+        }
     }
 }
